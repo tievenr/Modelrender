0981a32bef84fb9878e70c0325aed422b62c8465
tievenr <vineetraghu17@gmail.com>
2024-10-15 11:00:21 +0530

    added model 2b(fdx support)

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,71 @@
 import { useEffect } from 'react';
-
 import * as THREE from 'three';
 import { FBXLoader } from 'three/examples/jsm/loaders/FBXLoader';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
-
 import SceneInit from './lib/SceneInit';
 
-function App() {
-  useEffect(() => {
-    const test = new SceneInit('myThreeJsCanvas');
+function App ()
+{
+  useEffect( () =>
+  {
+    const test = new SceneInit( 'myThreeJsCanvas' );
     test.initialize();
     test.animate();
 
-    let loadedModel;
-    const glftLoader = new GLTFLoader();
-    glftLoader.load('./assets/shiba/scene.gltf', (gltfScene) => {
-      loadedModel = gltfScene;
-
-      gltfScene.scene.rotation.y = Math.PI / 8;
-      gltfScene.scene.position.y = 3;
-      gltfScene.scene.scale.set(10, 10, 10);
-      test.scene.add(gltfScene.scene);
-    });
-
-    const animate = () => {
-      if (loadedModel) {
-        loadedModel.scene.rotation.x += 0.01;
-        loadedModel.scene.rotation.y += 0.01;
-        loadedModel.scene.rotation.z += 0.01;
+    const gltfLoader = new GLTFLoader();
+    gltfLoader.load(
+      './assets/ichiban/scene.gltf',
+      ( gltfScene ) =>
+      {
+        gltfScene.scene.rotation.y = Math.PI;
+        gltfScene.scene.position.set( -10, -2, 0 );
+        gltfScene.scene.scale.set( 5, 5, 5 );
+        test.scene.add( gltfScene.scene );
+      },
+      ( xhr ) =>
+      {
+        console.log( ( xhr.loaded / xhr.total ) * 100 + '% loaded' );
+      },
+      ( error ) =>
+      {
+        console.log( 'Error loading GLTF model:', error );
       }
-      requestAnimationFrame(animate);
+    );
+
+    const fbxLoader = new FBXLoader();
+    fbxLoader.load(
+      'assets/Nier_2b/source/2b model rigging.fbx',
+      ( object ) =>
+      {
+        object.traverse( ( child ) =>
+        {
+          if ( child.isMesh )
+          {
+            child.material.transparent = false;
+          }
+        } );
+
+        object.position.set( 10, -1, 0 );
+        object.scale.set( 0.1, 0.1, 0.1 );
+        test.scene.add( object );
+      },
+      ( xhr ) =>
+      {
+        console.log( ( xhr.loaded / xhr.total ) * 100 + '% loaded' );
+      },
+      ( error ) =>
+      {
+        console.log( 'Error loading FBX model:', error );
+      }
+    );
+
+    const animate = () =>
+    {
+      requestAnimationFrame( animate );
     };
+
     animate();
-  }, []);
+  }, [] );
 
   return (
     <div>
